1c1f06b87b3101f38326edb09d2dea7d9899b1f6
James Benson <James.o.benson@gmail.com>
2016-03-11 13:41:24 -0600

    typos

--- password.py
+++ password.py
@@ -1,4 +1,19 @@
-""" To execute: python password.py
+""" To execute: python3 password.py
+#
+# Copyright 2016 James Benson
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
 """
 
 import getpass
@@ -7,43 +22,43 @@ from passlib.hash import pbkdf2_sha512
 from passlib.hash import sha512_crypt
 #import passlib.pwd.generate as generate
 
-print("Enter 1 for SHA256 salt size 16 Hash")
-print("Enter 2 for SHA512 salt size 16 Hash")
+print("Enter 1 for SHA256 salt size 16 hash")
+print("Enter 2 for SHA512 salt size 16 hash")
 print("Enter 3 for SHA512 (For ansible passwords)")
-#print("Enter 4 to create a Password -or- KeyFile")
+#print("Enter 4 to create a password -or- KeyFile")
 X = int(input("Enter 0 to quit the program.\n"))
 if X == 0:
     raise SystemExit
-    
+
 ###  This is function is in beta and the function currently does not work. Once updated,
 ###  this function should work.
 #if X == 4:
-#    NSize = int(input("How long would you like your Password? (# of characters)\n"))
-#    NEntropy = int(input("How much entropy would you like your Password? (Default: 48)\n"))
+#    NSize = int(input("How long would you like your password? (# of characters)\n"))
+#    NEntropy = int(input("How much entropy would you like your password? (Default: 48)\n"))
 #    NCount = int(input("How many passwords would you like? (Default: 2)\n"))
 #    generate(entropy=NEntropy, size=NSize, count=NCount, preset="diceware")
-#    print("Password is complete")
+#    print("password is complete")
 
-Y = str(input("Would you like the terminal to display the Password typed? Y/N\n"))
+Y = str(input("Would you like the terminal to display the password typed? Y/N\n"))
 if Y.lower() in ['y', 'ye', 'yes']:
-    Password = input("Please enter your Password:\n")
+    MY_PASSWORD = input("Please enter your password:\n")
 if Y.lower() in ['n', 'no']:
-    Password = getpass.getpass("Please enter your secret Password:")
+    MY_PASSWORD = getpass.getpass("Please enter your secret password:")
 if Y.lower() not in ['y', 'ye', 'yes', 'n', 'no']:
     raise SystemExit
 
 if X == 1:
-    Hash = pbkdf2_sha256.encrypt(Password, rounds=200000, salt_size=16)
-    print("The verification of your Password was:", pbkdf2_sha256.verify(Password, Hash))
-    print("Your Hash is: \n", Hash)
+    MY_HASH = pbkdf2_sha256.encrypt(MY_PASSWORD, rounds=200000, salt_size=16)
+    print("The verification of your password was:", pbkdf2_sha256.verify(MY_PASSWORD, MY_HASH))
+    print("Your hash is: \n", MY_HASH)
 elif X == 2:
-    Hash = pbkdf2_sha512.encrypt(Password, rounds=200000, salt_size=16)
-    print("The verification of your Password was:", pbkdf2_sha512.verify(Password, Hash))
-    print("Your Hash is: \n", Hash)
+    MY_HASH = pbkdf2_sha512.encrypt(MY_PASSWORD, rounds=200000, salt_size=16)
+    print("The verification of your password was:", pbkdf2_sha512.verify(MY_PASSWORD, MY_HASH))
+    print("Your hash is: \n", MY_HASH)
 elif X == 3:
-    Hash = sha512_crypt.encrypt(Password, rounds=200000, salt_size=16)
-    print("The verification of your Password was:", sha512_crypt.verify(Password, Hash))
-    print("Your Hash is: \n", Hash)
+    MY_HASH = sha512_crypt.encrypt(MY_PASSWORD, rounds=200000, salt_size=16)
+    print("The verification of your password was:", sha512_crypt.verify(MY_PASSWORD, MY_HASH))
+    print("Your hash is: \n", MY_HASH)
 else:
     print("Quitting")
     raise SystemExit
